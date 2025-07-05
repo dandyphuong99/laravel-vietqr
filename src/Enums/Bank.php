10113e66b37d0f8341dfb35c3311b634cfed50c6
dandyphuong99 <108283032+dandyphuong99@users.noreply.github.com>
2025-07-05 15:16:59 +0700

    Create Bank.php

---
+++ src/Enums/Bank.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Takashato\VietQr\Enums;
+
+enum Bank: string
+{
+    case ABBANK = '970425';
+    case ACB = '970416';
+    case AGRIBANK = '970405';
+    case BACABANK = '970409';
+    case BAOVIETBANK = '970438';
+    case BIDV = '970418';
+    case CAKE = '546034'; // Cake by VPBank
+    case CBBANK = '970444';
+    case DONGABANK = '970406';
+    case EXIMBANK = '970431';
+    case GPBANK = '970408';
+    case HDBANK = '970437';
+    case HOLABANK = '963388'; // Trước là TNEX
+    case IBK_HCM = '970434';
+    case IBK_HN = '970434';
+    case KASIKORNBANK = '796500';
+    case KIENLONGBANK = '970452';
+    case LIENVIETPOSTBANK = '970449';
+    case MB = '970422'; // MB Bank
+    case NAMABANK = '970428';
+    case NCB = '970419';
+    case OCEANBANK = '970414';
+    case OCB = '970448';
+    case PGBANK = '970430';
+    case PUBLICBANK = '970439';
+    case PVCOMBANK = '970412';
+    case SACOMBANK = '970403';
+    case SAIGONBANK = '970400';
+    case SCB = '970429';
+    case SEABANK = '970440';
+    case SHB = '970443';
+    case TECHCOMBANK = '970407';
+    case TPBANK = '970423';
+    case UOB = '970453';
+    case VIB = '970441';
+    case VIETABANK = '970427';
+    case VIETBANK = '970433';
+    case VIETCAPITALBANK = '970454';
+    case VIETCOMBANK = '970436';
+    case VIETINBANK = '970415';
+    case VPBANK = '970432';
+    case VRB = '970421'; // Vietnam - Russia Bank
+    case WOORIBANK = '970457';
+}
